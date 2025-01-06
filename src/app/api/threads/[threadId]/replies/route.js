faad253b55a36d157a9ccc4ef98ac73d29c2b234
A B S Talha <abstalha192@gmail.com>
2025-01-06 19:31:21 +0600

    wip2

--- src/app/api/threads/[threadId]/replies/route.js
+++ src/app/api/threads/[threadId]/replies/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import connectDB from '@/lib/mongodb';
-import Thread from '@/models/Thread';
+import connectDB from '@/app/lib/mongodb';
+import Thread from '@/app/moduls/Thread';
 
 export async function POST(req, { params }) {
   try {
